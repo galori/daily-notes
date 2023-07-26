05e9b6755a5cd3816e57481b0776ef3315a6ce8a
Gall Steinitz <gall.steinitz@datagrail.io>
2023-07-26 10:30:01 -0700

    initial commit after "bundler gem daily_notes"

---
+++ lib/daily_notes.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "daily_notes/version"
+
+module DailyNotes
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/daily_notes/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DailyNotes
+  VERSION = "0.1.0"
+end
