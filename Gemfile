06a65646869c95347fb72e48581588c00c7e479e
Gall Steinitz <gall.steinitz@datagrail.io>
2025-02-20 09:47:05 -0800

    improved rbenv integration

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@
 
 source "https://rubygems.org"
 
-ruby '3.1.6'
+ruby '3.3.7'
 
 # Specify your gem's dependencies in daily_notes.gemspec
 gemspec
--- bin/create_daily_notes.sh
+++ bin/create_daily_notes.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-export HOME=/users/gall
-if [ -d $HOME/.rbenv ]; then
-  export PATH="$HOME/.rbenv/shims:$PATH"
+
+# Ensure rbenv's bin is in PATH
+export PATH="$HOME/.rbenv/bin:$PATH"
+
+# Initialize rbenv; this sets up shims for Ruby versions
+if command -v rbenv 1>/dev/null 2>&1; then
   eval "$(rbenv init -)"
 fi
 
 cd /Users/gall/workspace/daily-notes
-bundle exec "bin/create_daily_notes.rb" "/users/gall/Documents/Notes/Daily/"
-
-#"/users/gall/workspace/daily-notes/bin/create_daily_notes" "/users/gall/Documents/Notes/Daily/"
-
-
+bundle exec bin/create_daily_notes.rb "/users/gall/Documents/Notes/Daily/"
